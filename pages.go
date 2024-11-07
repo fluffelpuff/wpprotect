bc706f07d4b391433712d1342d6d01ecf4382706
Fluffel₿uff ⚡️ <113269946+fluffelpuff@users.noreply.github.com>
2024-11-07 10:28:02 +0000

    update

--- pages.go
+++ pages.go
@@ -13,11 +13,23 @@ var webFiles embed.FS
 
 var templates *template.Template
 
+type PageData struct {
+	Title       string
+	Lang        string
+	BsThemeDark string
+}
+
 func initPages() {
 	var err error
 	templates, err = template.ParseFS(webFiles, "web/*.html")
 	if err != nil {
 		log.Fatalf("Fehler beim Parsen der Templates: %v", err)
+	}
+
+	// Seiten Daten
+	data := PageData{
+		BsThemeDark: "auto",
+		Lang:        "de",
 	}
 
 	// Geladene Templates auflisten
@@ -32,6 +44,10 @@ func initPages() {
 		switch r.URL.Path {
 		case "/logon", "/logon.html":
 			tmplName = "logon.html"
+			data.Title = "Anmeldung"
+		case "/register", "/register.html":
+			tmplName = "logon.html"
+			data.Title = "Registrieren"
 		default:
 			http.NotFound(w, r)
 			return
@@ -41,7 +57,7 @@ func initPages() {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
 		// Template ausführen
-		err := templates.ExecuteTemplate(w, tmplName, nil)
+		err := templates.ExecuteTemplate(w, tmplName, data)
 		if err != nil {
 			log.Printf("Fehler beim Ausführen des Templates %s: %v", tmplName, err)
 			http.Error(w, "Fehler beim Ausführen des Templates: "+err.Error(), http.StatusInternalServerError)
@@ -54,5 +70,7 @@ func initPages() {
 	if err != nil {
 		log.Fatalf("Fehler beim Zugriff auf CSS-Dateien: %v", err)
 	}
+
+	// Stellt die CSS Dateien bereit
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.FS(cssFiles))))
 }
