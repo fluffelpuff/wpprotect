cdb4210af54ad4067987eaf560a73b6348ed078c
Fluffel₿uff ⚡️ <113269946+fluffelpuff@users.noreply.github.com>
2024-11-07 09:15:28 +0000

    add logon page

--- pages.go
+++ pages.go
@@ -9,10 +9,35 @@ import (
 var webFiles embed.FS
 
 func initPages() {
-	// Handler für die Startseite (root path /) - lädt die index.html manuell
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Datei lesen
 		data, err := webFiles.ReadFile("web/index.html")
+		if err != nil {
+			http.Error(w, "Datei nicht gefunden", http.StatusNotFound)
+			return
+		}
+
+		// Content-Type setzen und Datei senden
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(data)
+	})
+
+	http.HandleFunc("/logon", func(w http.ResponseWriter, r *http.Request) {
+		// Datei lesen
+		data, err := webFiles.ReadFile("web/logon.html")
+		if err != nil {
+			http.Error(w, "Datei nicht gefunden", http.StatusNotFound)
+			return
+		}
+
+		// Content-Type setzen und Datei senden
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(data)
+	})
+
+	http.HandleFunc("/css/modal.css", func(w http.ResponseWriter, r *http.Request) {
+		// Datei lesen
+		data, err := webFiles.ReadFile("web/css/modal.css")
 		if err != nil {
 			http.Error(w, "Datei nicht gefunden", http.StatusNotFound)
 			return
